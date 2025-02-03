7be755e7e91de6c9c1d75025193808deffc7eada
Wei-Ting Yap <29475856+weitingyap@users.noreply.github.com>
2025-02-03 15:30:27 +0800

    feat: add basic player factory and create two players

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log("hello world!");
+function createPlayer(name){
+    const score = 0;
+    return {name, score};
+}
+
+playerOne = createPlayer("One");
+playerTwo = createPlayer("Two");
+
+console.log(playerOne);
