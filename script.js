de36ab1888aca1ecb8dd2061ffb7e965450d4c26
Wei-Ting Yap <29475856+weitingyap@users.noreply.github.com>
2025-02-03 22:45:25 +0800

    feat: add method to check for win on the board

--- script.js
+++ script.js
@@ -23,8 +23,38 @@ const game = (function(){
         nextPlayerNum = nextPlayerNum === 1 ? 2 : 1; // update next player
     };
 
+    const checkWin = function(){
+        
+        function checkHorizontal(){
+            return (board.some((row)=>{
+                return row.every((cell)=>{
+                    return cell === nextPlayerNum});
+            }));
+        }
+
+        function checkVertical(){
+            for (let x = 0; x < 3; x++){
+                let colMoveCnt = 0;
+                for (let y = 0; y < 3; y++){
+                    if (board[x][y] === nextPlayerNum) colMoveCnt++;
+                }
+                if (colMoveCnt === 3) return true;
+            }
+            return false;
+        }
+
+        function checkDiagonal(){
+            return (
+                (board[0][0] === nextPlayerNum && board[1][1] === nextPlayerNum && board[2][2] === nextPlayerNum) ||
+                (board[0][2] === nextPlayerNum && board[1][1] === nextPlayerNum && board[2][0] === nextPlayerNum)
+            );
+        }
+
+        return (checkHorizontal() || checkVertical() || checkDiagonal());
+    };
+
     playerOne = createPlayer("One", 1);
     playerTwo = createPlayer("Two", 2);
 
-    return {board, placeMove};
+    return {board, placeMove, checkWin};
 })();
