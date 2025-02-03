d2bc5daa675c603d9a08aba5aba0233ee46fb145
Wei-Ting Yap <29475856+weitingyap@users.noreply.github.com>
2025-02-03 15:43:36 +0800

    feat: place move on board given playerNum, coordinates

--- script.js
+++ script.js
@@ -1,10 +1,26 @@
-function createPlayer(name){
+const game = (function(){
+
+    // 0 means empty, 1 is player 1, 2 is player 2
+    let board = [
+        [0, 0, 0],
+        [0, 0, 0],
+        [0, 0, 0]
+    ];
+
+    const placeMove = function(x, y, playerNum){
+        board[x][y] = playerNum;
+    };
+
+    return {board, placeMove};
+})();
+
+function createPlayer(name, playerNum){
     let score = 0;
 
     const updateScore = () => {score++};
 
-    return {name, score, updateScore};
+    return {name, playerNum, score, updateScore};
 }
 
-playerOne = createPlayer("One");
-playerTwo = createPlayer("Two");
+playerOne = createPlayer("One", 1);
+playerTwo = createPlayer("Two", 2);
