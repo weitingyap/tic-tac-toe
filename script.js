bdfa7e2bf4e7f397d33ab48dee18e21571d9ae24
Wei-Ting Yap <29475856+weitingyap@users.noreply.github.com>
2025-02-03 23:18:12 +0800

    feat: add initialize board function

--- script.js
+++ script.js
@@ -12,12 +12,21 @@ const game = (function(){
     
     const players = [createPlayer("One", 1), createPlayer("Two", 2)];
 
-    // 0 means empty, 1 is player 1, 2 is player 2
-    let board = [
-        [0, 0, 0],
-        [0, 0, 0],
-        [0, 0, 0]
-    ];
+
+    // create and initialize board
+    let board;
+
+    const initBoard = function(){
+        // 0 means empty, 1 is player 1, 2 is player 2
+        board = [
+            [0, 0, 0],
+            [0, 0, 0],
+            [0, 0, 0]
+        ];
+    }
+
+    initBoard();
+
 
     // turn counter which has value set to playerNum of next player
     let [currPlayer, nextPlayer] = [1, 2];
